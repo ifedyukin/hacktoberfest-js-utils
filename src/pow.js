a01c4916b1fdb0c63f51cde27d1e9f7207ca0ada
Joris MANSART <joris.mansart@alcuin.com>
2018-10-16 23:35:31 +0200

    Fix #9 implemented pow and added test for method sign

--- src/pow.js
+++ src/pow.js
@@ -1,3 +1,15 @@
-export default (number, power) => {
-  // TO IMPLEMENT IN ANOTHER PR
+const pow = (number, power) => {
+
+    if (power === 0) return 1;
+
+    let recPow = pow(number, power / 2);
+
+    // check if odd
+    if (power & 1) {
+        return power > 0 ? number * recPow * recPow 
+        : recPow * recPow / number;
+    }
+    return recPow * recPow;
 }
+
+export default pow;
