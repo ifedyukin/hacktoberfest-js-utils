706338a7a166e9954a9baf494ab0ebb041ea0d8e
Alex Bakoushin <bakoushin@Alexs-Air.lan>
2018-10-17 22:27:41 +0300

    Add Atbash challenge

---
+++ __tests__/atbash.test.js
@@ -0,0 +1,31 @@
+import atbash from '../src/atbash';
+
+describe('abs', () => {
+  test('Correct alphabet substitution', () => {
+    expect(atbash('ABCDEFGHIJKLMNOPQRSTUVWXYZ')).toBe(
+      'ZYXWVUTSRQPONMLKJIHGFEDCBA',
+    );
+  });
+
+  test('Correct reverse alphabet substitution', () => {
+    expect(atbash('ZYXWVUTSRQPONMLKJIHGFEDCBA')).toBe(
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    );
+  });
+
+  test('Correct encoding: HACKTOBERFEST => SZXPGLYVIUVHG', () => {
+    expect(atbash('HACKTOBERFEST')).toBe('SZXPGLYVIUVHG');
+  });
+
+  test('Correct decoding: TRGSFY => GITHUB', () => {
+    expect(atbash('TRGSFY')).toBe('GITHUB');
+  });
+
+  test('Correct encoding: HOLD => SLOW', () => {
+    expect(atbash('HOLD')).toBe('SLOW');
+  });
+
+  test('Correct decoding: ZOO => ALL', () => {
+    expect(atbash('ZOO')).toBe('ALL');
+  });
+});
---
+++ src/atbash.js
@@ -0,0 +1,17 @@
+const atbash = str => {
+  // Implement Atbash cipher/deciper for Latin alphabet.
+  //
+  // Atbash is an old Hebrew substitution cipher, used in Bible.
+  // https://en.wikipedia.org/wiki/Atbash
+  //
+  // In the Atbash cipher, alphabet is mapped to its reverse, so the first letter
+  // becomes the last letter, the second letter becomes the second to last letter,
+  // and so on.
+  //
+  // Atbash mapping for Latin alphabet:
+  //
+  // A|B|C|D|E|F|G|H|I|J|K|L|M
+  // Z|Y|X|W|V|U|T|S|R|Q|P|O|N
+};
+
+export default atbash;
