d4a4cd090605ca0f585ee428ca21268fd4f0e1db
Diwaker Singh <diwaker.singh@inmobi.com>
2018-10-17 21:51:48 +0530

    Implemented gcd, corrected gcd tests and added body for xadder function

--- src/gcd.js
+++ src/gcd.js
@@ -1,5 +1,10 @@
 const gcd = (first, second) => {
-//function should return gcd of two numbers first and second
+    //function should return gcd of two numbers first and second
+    if (!second) {
+        return first;
+    }
+
+    return gcd(second, first % second);
 }
 
 export default gcd;
