1e7f811d0d0c748a1c256c95ecf6aadfab2e5a27
Alex Bakoushin <bakoushin@Alexs-Air.lan>
2018-10-17 21:47:31 +0300

    Implement capitalize()

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,9 +1,5 @@
 const capitalize = str => {
-  /*
-   takes a string and a capitalize the first letter, lower all the others
-   
-   eg: capitalize('blaBlaCar') results in this: 'Blablacar'
-  */
+  return str[0].toUpperCase() + str.slice(1).toLowerCase();
 };
 
 export default capitalize;
