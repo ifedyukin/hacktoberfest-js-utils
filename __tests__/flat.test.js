2a361f0cbd53be88e6dab9ea50f63394963e22ee
Aakash Singh <singh.aakash1801@gmail.com>
2018-10-19 15:28:51 +0530

    updated nestedList

--- __tests__/flat.test.js
+++ __tests__/flat.test.js
@@ -1,14 +1,9 @@
 import flat from '../src/flat';
 
 describe('flat', () => {
-  let nestedList=[];
+  let nestedList=[ [1, 2, 3],[1, 2, 3, [4, 5]], [1, 2, 3, [4, 5, [6, 7, [8]]]] ,[1, 2, 3, [4, 5, 6, 7, 8]]];
 
-  beforeEach(() => {
-    nestedList[0] = [1, 2, 3];
-    nestedList[1] = [1, 2, 3, [4, 5]];
-    nestedList[2] = [1, 2, 3, [4, 5, [6, 7, [8]]]];
-    nestedList[3] = [1, 2, 3, [4, 5, 6, 7, 8]];
-  });
+
 
   test('should return empty list', () => {
     expect(flat([])).toBe([]);
