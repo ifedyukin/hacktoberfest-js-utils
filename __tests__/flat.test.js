34bc4788f0ff77f8b7b92d7a7a161ac8bd19f76b
Aakash Singh <singh.aakash1801@gmail.com>
2018-10-19 15:40:45 +0530

    change toBe method to toEqual

--- __tests__/flat.test.js
+++ __tests__/flat.test.js
@@ -6,22 +6,22 @@ describe('flat', () => {
 
 
   test('should return empty list', () => {
-    expect(flat([])).toBe([]);
-     expect(flat([]).length).toBe(0);
+    expect(flat([])).toEqual([]);
+     expect(flat([]).length).toEqual(0);
   });
 
   test('should return the same value is pass the same argument', () => {
-    expect(flat(  nestedList[0])).toBe([1,2,3]);
+    expect(flat(  nestedList[0])).toEqual([1,2,3]);
   });
 
   test('should return flat list with all elements on same level', () => {
-    expect(flat(nestedList[1])).toBe([1, 2, 3, 4, 5]);
+    expect(flat(nestedList[1])).toEqual([1, 2, 3, 4, 5]);
   });
 
   test('should return flat list with all elements on same level', () => {
-    expect(flat(nestedList[0]).length).toBe(3);
-    expect(flat(nestedList[1]).length).toBe(5);
-    expect(flat(nestedList[2]).length).toBe(8);
-    expect(flat(nestedList[3]).length).toBe(8);
+    expect(flat(nestedList[0]).length).toEqual(3);
+    expect(flat(nestedList[1]).length).toEqual(5);
+    expect(flat(nestedList[2]).length).toEqual(8);
+    expect(flat(nestedList[3]).length).toEqual(8);
   });
 });
