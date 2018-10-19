1464c9a960798b2f847248507b1d30b5b8555855
Aakash Singh <singh.aakash1801@gmail.com>
2018-10-19 16:37:22 +0530

    Implement map function
    Implemented map function

--- src/map.js
+++ src/map.js
@@ -1,5 +1,15 @@
 const map = function(arr, applyFunction) {
   // Should apply applyFunction to every element in array
+  
+     const mappedArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let mapped = applyFunction(arr[i]);
+        mappedArr.push(mapped);
+    }
+    return mappedArr;
+  
+  
+  
 };
 
 export default map;
