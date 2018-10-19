ce4c0fab89d60c4fd6b82fad5f5834ea40058868
Markus Pettersson <markp@student.chalmers.se>
2018-10-19 09:29:51 +0200

    Added skeleton for flat function and created test cases.

---
+++ __tests__/flat.test.js
@@ -0,0 +1,28 @@
+import flat from '../src/flat';
+
+describe('flat', () => {
+  let nestedList;
+
+  beforeEach(() => {
+    nestedList0 = [1, 2, 3];
+    nestedList1 = [1, 2, 3, [4, 5]];
+    nestedList2 = [1, 2, 3, [4, 5, [6, 7, [8]]]];
+    nestedList3 = [1, 2, 3, [4, 5, 6, 7, 8]];
+  });
+
+  test('should return empty list', () => {
+    expect(flat([])).toBe([]);
+  });
+
+  test('should return the same value is pass the same argument', () => {
+    expect(flat(nestedList0)).toBe(nestedList0);
+  });
+
+  test('should return flat list with all elements on same level', () => {
+    expect(flat(nestedList1)).toBe([1, 2, 3, 4, 5]);
+  });
+
+  test('should return flat list with all elements on same level', () => {
+    expect(flat(nestedList2)).toBe(flat(nestedList3));
+  });
+});
---
+++ src/flat.js
@@ -0,0 +1,6 @@
+const flat = list => {
+  // Function takes a list as argument, should return a list with all
+  // elements from passed list at level 0
+};
+
+export default flat;
