a382d4b684d076bd6e686e58206716c91a74e6be
ikhnaton <ikhnaton@techie.com>
2018-10-19 04:08:00 -0400

    implement flat and fix tests to be valid tests

--- __tests__/flat.test.js
+++ __tests__/flat.test.js
@@ -1,28 +1,26 @@
 import flat from '../src/flat';
 
 describe('flat', () => {
-  let nestedList;
+  let nestedList = [];
 
   beforeEach(() => {
-    nestedList0 = [1, 2, 3];
-    nestedList1 = [1, 2, 3, [4, 5]];
-    nestedList2 = [1, 2, 3, [4, 5, [6, 7, [8]]]];
-    nestedList3 = [1, 2, 3, [4, 5, 6, 7, 8]];
+    nestedList[0] = [1, 2, 3];
+    nestedList[1] = [1, 2, 3, [4, 5]];
+    nestedList[2] = [1, 2, 3, [4, 5, [6, 7, [8]]]];
+    nestedList[3] = [1, 2, 3, [4, 5, 6, 7, 8]];
   });
 
   test('should return empty list', () => {
-    expect(flat([])).toBe([]);
+    expect(flat([]).length).toBe(0);
   });
 
   test('should return the same value is pass the same argument', () => {
-    expect(flat(nestedList0)).toBe(nestedList0);
-  });
-
-  test('should return flat list with all elements on same level', () => {
-    expect(flat(nestedList1)).toBe([1, 2, 3, 4, 5]);
-  });
-
-  test('should return flat list with all elements on same level', () => {
-    expect(flat(nestedList2)).toBe(flat(nestedList3));
+    expect(flat(nestedList[0]).length).toBe(3);
+    expect(flat(nestedList[1]).length).toBe(5);
+    expect(flat(nestedList[2]).length).toBe(8);
+    expect(flat(nestedList[3]).length).toBe(8);
+    nestedList.forEach(arr => {
+      arr.forEach((element, index) => expect(element).toBe(arr[index]));
+    });
   });
 });
