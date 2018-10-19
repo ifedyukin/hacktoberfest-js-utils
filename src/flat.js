a6da34bf93b8a3ce4abba2e1eac0a6ba6a7e6ab7
Aakash Singh <singh.aakash1801@gmail.com>
2018-10-19 14:12:52 +0530

    Implementation of flat function
    Implemented flat function using recursion

--- src/flat.js
+++ src/flat.js
@@ -1,6 +1,7 @@
 const flat = list => {
   // Function takes a list as argument, should return a list with all
   // elements from passed list at level 0
+   return list.reduce((acc, val) => Array.isArray(val) ? acc.concat(flat(val)) : acc.concat(val), []);
 };
 
 export default flat;
